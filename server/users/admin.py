624098e001c72d95f1978e8ddbf1db34ca46fab3
a-paudel <ankur@paudel.me>
2024-01-28 22:35:13 +0000

    Create management command to create groups

--- server/users/admin.py
+++ server/users/admin.py
@@ -1,7 +1,14 @@
+from typing import Any
 from django.contrib import admin
+from django.contrib.auth.hashers import make_password
+from django.forms import Form
+from django.http import HttpRequest
 from unfold.admin import ModelAdmin
 from users.models import User, Group
 from django.contrib.auth.models import Group as _Group
+from django.contrib.auth.admin import UserAdmin
+from django.db import models
+from django import forms
 
 # Register your models here.
 admin.site.unregister(_Group)
@@ -12,9 +19,26 @@ admin.site.unregister(_Group)
 class UserAdmin(ModelAdmin):
     search_fields = ["username"]
     list_display = ["username", "is_staff"]
-    pass
+    # fields = ["username", "password", "groups", "superuser_status", ]
+
+    formfield_overrides = {
+        models.ManyToManyField: {"widget": forms.CheckboxSelectMultiple},
+    }
+
+    def save_model(self, request: HttpRequest, obj: User, form: Form, change: Any) -> None:
+        user = form.save(commit=False)  # type: ignore
+        user.password = make_password(form.cleaned_data["password"])
+        user.save()
+        # return super().save_model(request, obj, form, change)
 
 
 @admin.register(Group)
 class GroupAdmin(ModelAdmin):
     model = Group
+
+    formfield_overrides = {
+        models.ManyToManyField: {"widget": forms.CheckboxSelectMultiple},
+    }
+
+
+# create groups for Owner, Manager, Employee and Customer
---
+++ server/users/management/commands/creategroups.py
@@ -0,0 +1,49 @@
+from hmac import new
+from users.models import Group
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Permission
+
+GROUPS = {
+    "owner": {
+        "user": ["add", "change", "delete", "view"],
+        "group": ["add", "change", "delete", "view"],
+        "category": ["add", "change", "delete", "view"],
+        "product": ["add", "change", "delete", "view"],
+        "Image": ["add", "change", "delete", "view"],
+    },
+    "manager": {
+        "user": ["add", "change", "view"],
+        "group": ["view"],
+        "category": ["add", "change", "view"],
+        "product": ["add", "change", "view"],
+        "Image": ["add", "change", "view"],
+    },
+    "employee": {
+        "user": ["add", "change", "view"],
+        "group": ["view"],
+        "category": ["change", "view"],
+        "product": ["change", "view", "add"],
+        "Image": ["change", "view", "add"],
+    },
+    "customer": {
+        "category": ["view"],
+        "product": ["view"],
+        "Image": ["view"],
+    },
+}
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        for group, data in GROUPS.items():
+            new_group, created = Group.objects.get_or_create(name=group)
+
+            for model, perms in data.items():
+                for perm in perms:
+                    name = f"Can {perm} {model}"
+                    _perm = Permission.objects.filter(name=name).first()
+                    if _perm:
+                        new_group.permissions.add(_perm)
+
+
+# _create_groups()
